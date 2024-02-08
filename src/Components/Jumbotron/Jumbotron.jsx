505fed69ed3fefe5ec1d7bbc07582b80c51c5df1
Harsh Ninave <harshninave58@gmail.com>
2024-02-08 23:44:27 +0530

    component for email purpose

---
+++ src/Components/Jumbotron/Jumbotron.jsx
@@ -0,0 +1,49 @@
+function Jumbotron() {
+  return (
+    <section className="bg-white dark:bg-gray-900 bg-[url('your-image-url.jpg')] dark:bg-[url('your-dark-image-url.jpg')]">
+      <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 z-10 relative">
+        <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+          Welcome to My Portfolio
+        </h1>
+        <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-200">
+          Explore my skills, projects, and journey in the world of technology and innovation.
+        </p>
+        <form className="w-full max-w-md mx-auto">
+          <label
+            htmlFor="portfolioEmail"
+            className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">
+            Stay Updated
+          </label>
+          <div className="relative">
+            <div className="absolute inset-y-0 rtl:inset-x-0 start-0 flex items-center ps-3.5 pointer-events-none">
+              <svg
+                className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 20 16">
+                <path d="m10.036 8.278 9.258-7.79A1.979 1.979 0 0 0 18 0H2A1.987 1.987 0 0 0 .641.541l9.395 7.737Z" />
+                <path d="M11.241 9.817c-.36.275-.801.425-1.255.427-.428 0-.845-.138-1.187-.395L0 2.6V14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2.5l-8.759 7.317Z" />
+              </svg>
+            </div>
+            <input
+              type="email"
+              id="portfolio-email"
+              className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-white focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Enter your email for updates..."
+              required
+            />
+            <button
+              type="submit"
+              className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+              Subscribe
+            </button>
+          </div>
+        </form>
+      </div>
+      <div className=" from-blue-50 to-transparent dark:from-blue-900 w-full h-full absolute top-0 left-0 z-0"></div>
+    </section>
+  );
+}
+
+export default Jumbotron;
